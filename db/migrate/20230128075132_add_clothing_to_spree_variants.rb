733780eb17a51e49dcae37b110f16dd0ef2d3c96
Marie Lamoureux <lamoureuxmarie@outlook.com>
2023-02-02 21:09:30 +0100

    clothing attribute

--- db/migrate/20230128075132_add_clothing_to_spree_variants.rb
+++ db/migrate/20230128075132_add_clothing_to_spree_variants.rb
@@ -1,5 +1,5 @@
 class AddClothingToSpreeVariants < ActiveRecord::Migration[7.0]
   def change
-    add_column :spree_variants, :clothing, :boolean
+    add_column :spree_variants, :clothing, :boolean, default: true
   end
 end
